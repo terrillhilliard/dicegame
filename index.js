3bf14a2c97374bcc0141e8f20cecdba2aadbd053
Terrill Hilliard <119083888+terrillhilliard@users.noreply.github.com>
2022-12-14 11:56:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 
 
-var randomNumber1 = math.floor(math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var image1 = document.querySelector("img1")[0];
+var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", "randomDiceImage");
+image1.setAttribute("src", randomDiceImage);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2)
+
